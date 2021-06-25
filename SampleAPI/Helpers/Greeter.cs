8e9da9b5e919932a880bf881e59b1af4d8c85d78
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-25 13:21:10 +0700

    menambahkan IGreeter

---
+++ SampleAPI/Helpers/Greeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Helpers
+{
+    public class Greeter : IGreeter
+    {
+        public string GetMessage()
+        {
+            return "Salam dari ASP.NET Core Developer";
+        }
+    }
+}
---
+++ SampleAPI/Helpers/IGreeter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Helpers
+{
+    public interface IGreeter
+    {
+        string GetMessage();
+    }
+}
